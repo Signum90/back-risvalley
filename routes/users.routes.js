e2af69779a4e9e130ed24a2d63673553a405b018
juanPabloM <juan.martinez@cpcoriente.org>
2024-02-16 09:29:30 -0500

    +mensajes de validacion personalizados

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -39,24 +39,24 @@ router.post("/create", multerConfig.upload.single('logo'), [
 
 
 router.post("/create/dashboard", Middlewares.validateAdminMiddleware, multerConfig.upload.single('logo'), [
-  check('email').trim().notEmpty().isEmail().custom(async (email) => {
+  check('email').trim().notEmpty().withMessage(customMessages.required).isEmail().withMessage(customMessages.email).custom(async (email) => {
     const exists = await validateFieldUnique('user', 'email', email)
     if (exists) return Promise.reject('El correo electronico ya se encuentra registrado');
   }),
-  check('nombreEntidad').trim().isString().isLength({ max: 120 }).custom(async (nombre, { req }) => {
+  check('nombreEntidad').trim().isString().withMessage(customMessages.string).isLength({ max: 120 }).withMessage(customMessages.length).custom(async (nombre, { req }) => {
     if (req.body.tipo != 1) {
       const exists = await validateFieldUnique('entidad', 'nombre', nombre)
       if (exists) return Promise.reject('Ya existe una entidad con ese nombre');
     }
   }),
-  body('nombre').trim().notEmpty().withMessage(customMessages.required).isString().withMessage(customMessages.string).isLength({ max: 40 }),
-  check('cargo').trim().notEmpty().isString().isLength({ max: 70 }),
-  check('telefono').trim().optional({ nullable: true }).isInt().isLength({ max: 11 }),
-  check('tipo').notEmpty().isInt({ min: 1, max: 4 }),
-  check('descripcion').trim().isString().isLength({ max: 80 }).custom(async (descripcion, { req }) => {
+  body('nombre').trim().notEmpty().withMessage(customMessages.required).isString().withMessage(customMessages.string).isLength({ max: 40 }).withMessage(customMessages.length),
+  check('cargo').trim().notEmpty().withMessage(customMessages.required).isString().withMessage(customMessages.string).isLength({ max: 70 }).withMessage(customMessages.length),
+  check('telefono').trim().optional({ nullable: true }).isInt().withMessage(customMessages.int).isLength({ max: 11 }).withMessage(customMessages.length),
+  check('tipo').notEmpty().withMessage(customMessages.required).isInt({ min: 1, max: 4 }).withMessage(customMessages.int),
+  check('descripcion').trim().isString().withMessage(customMessages.string).isLength({ max: 80 }).withMessage(customMessages.length).custom(async (descripcion, { req }) => {
     if (req.body.tipo != 1 && !descripcion) return Promise.reject('La descripcion de la entidad es obligatoria');
   }),
-  check('sigla').trim().isString().isLength({ max: 10 }).custom(async (sigla, { req }) => {
+  check('sigla').trim().isString().withMessage(customMessages.string).isLength({ max: 10 }).withMessage(customMessages.length).custom(async (sigla, { req }) => {
     if (req.body.tipo != 1 && !sigla) return Promise.reject('Las siglas de la entidad son obligatoria');
   }),
   check('tipoEntidad').custom(async (tipoEntidad, { req }) => {
@@ -65,19 +65,19 @@ router.post("/create/dashboard", Middlewares.validateAdminMiddleware, multerConf
   check('idTipoNaturalezaJuridica').custom(async (tipoNaturaleza, { req }) => {
     if (req.body.tipo != 1 && !tipoNaturaleza) return Promise.reject('El tipo de naturaleza de la entidad es obligatorio');
   }),
-  check('direccion').trim().isString().isLength({ max: 80 }).custom(async (direccion, { req }) => {
+  check('direccion').trim().isString().withMessage(customMessages.string).isLength({ max: 80 }).withMessage(customMessages.length).custom(async (direccion, { req }) => {
     if (req.body.tipo != 1 && !direccion) return Promise.reject('La direccion de la entidad es obligatoria');
   }),
-  check('telefonoEntidad').trim().isLength({ max: 11 }).custom(async (telefono, { req }) => {
+  check('telefonoEntidad').trim().isLength({ max: 11 }).withMessage(customMessages.length).custom(async (telefono, { req }) => {
     if (req.body.tipo != 1 && !telefono) return Promise.reject('El telefono de la entidad es obligatorio');
   }),
-  check('emailEntidad').trim().isEmail().custom(async (email, { req }) => {
+  check('emailEntidad').trim().custom(async (email, { req }) => {
     if (req.body.tipo != 1 && !email) return Promise.reject('El email de la entidad es obligatorio');
   }),
-  check('urlDominio').trim().isString().isLength({ max: 80 }),
-  check('urlFacebook').trim().isString().isLength({ max: 80 }),
-  check('urlTwitter').trim().isString().isLength({ max: 80 }),
-  check('urlLinkedin').trim().isString().isLength({ max: 80 }),
+  check('urlDominio').trim().isString().withMessage(customMessages.string).isLength({ max: 80 }).withMessage(customMessages.length),
+  check('urlFacebook').trim().isString().withMessage(customMessages.string).isLength({ max: 80 }).withMessage(customMessages.length),
+  check('urlTwitter').trim().isString().withMessage(customMessages.string).isLength({ max: 80 }).withMessage(customMessages.length),
+  check('urlLinkedin').trim().isString().withMessage(customMessages.string).isLength({ max: 80 }).withMessage(customMessages.length),
   Middlewares.scan_errors
 ], usersController.registerUser);
 
