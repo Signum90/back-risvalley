54043987c8ec3bf2a37d726216a6d02adfd75e11
juanPabloM <juan.martinez@cpcoriente.org>
2024-02-23 14:51:28 -0500

    - autenticacion listar tipos

--- routes/types.routes.js
+++ routes/types.routes.js
@@ -17,12 +17,12 @@ const router = Router();
 
 //■► RUTEO: ===================================== ◄■:
 router.post("/create", Middlewares.validateJWTMiddleware, async (req, res) => await typesController.saveTypes(req, res));
-router.get("/list", Middlewares.validateJWTMiddleware, [
-    query('tipo')
-        .trim()
-        .notEmpty().withMessage('El campo "tipo" es requerido')
-        .isInt({ min: 1, max: 3 }).withMessage('El campo "tipo" debe ser un número entero entre 1 y 3'),
-    Middlewares.scan_errors
+router.get("/list", [
+  query('tipo')
+    .trim()
+    .notEmpty().withMessage('El campo "tipo" es requerido')
+    .isInt({ min: 1, max: 3 }).withMessage('El campo "tipo" debe ser un número entero entre 1 y 3'),
+  Middlewares.scan_errors
 ], async (req, res) => await typesController.getTypes(req, res));
 
 module.exports = router;
