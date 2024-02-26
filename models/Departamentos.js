f213f7643a59e7ad3101d4d4e4443f0f55e695c1
juanPabloM <juan.martinez@cpcoriente.org>
2024-02-26 09:16:04 -0500

    ajuste model departamentos

--- models/Departamentos.js
+++ models/Departamentos.js
@@ -1,28 +1,28 @@
 const { DataTypes, Model } = require('sequelize');
 
 class DepartamentosModel extends Model {
-    static initialize(sequelizeInstace) {
-        return super.init(
-            {
-                id: {
-                    type: DataTypes.MEDIUMINT.UNSIGNED,
-                    primaryKey: true,
-                    autoIncrement: true
-                },
-                nombre: {
-                    type: DataTypes.STRING(25),
-                    allowNull: false
-                },
-            },
-            {
-                sequelize: sequelizeInstace,
-                modelName: 'Departamentos',
-                timestamps: false,
-                tableName: 'departamentos'
-            },
+  static initialize(sequelizeInstace) {
+    return super.init(
+      {
+        id: {
+          type: DataTypes.MEDIUMINT.UNSIGNED,
+          primaryKey: true,
+          //autoIncrement: true
+        },
+        nombre: {
+          type: DataTypes.STRING(25),
+          allowNull: false
+        },
+      },
+      {
+        sequelize: sequelizeInstace,
+        modelName: 'Departamentos',
+        timestamps: false,
+        tableName: 'departamentos'
+      },
 
-        )
-    }
+    )
+  }
 }
 
 //■► EXPORTAR:  ◄■:
