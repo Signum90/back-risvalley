27182ec159067e5324991de24daec2285522db49
juanPHome <juan.martinez@cpcoriente.org>
2024-03-20 14:21:24 -0500

    :hammer:

--- config/config.js
+++ config/config.js
@@ -1,39 +1,13 @@
 // #########################################
 // ############### CONFIG #################
 // #########################################
-// const config_db = {
-//   host: process.env.HOST ?? 'localhost',
-//   username: process.env.USERDB ?? "cpcoriente_rispark",
-//   password: process.env.PASSDB ?? "Rispark_:bd2024",
-//   database: process.env.DATABASE ?? "cpcoriente_risparkbd",
-//   dialect: process.env.DIALECT ?? "mysql",
-//   secret: process.env.SECRETKEYJWT ?? '',
-//   urlFiles: `http://${process.env.HOST}:${process.env.PORT}/api/archivos/`,
-//   email: {
-//     smtp: {
-//       service: 'gmail',
-//       host: 'smtp.gmail.com' ?? process.env.MAIL_HOST,
-//       port: 587 ?? process.env.MAIL_PORT,
-//       secure: false,
-//       auth: {
-//         user: 'juan.martinez@cpcoriente.org' ?? process.env.MAIL_USERNAME,
-//         pass: 'bjif ptby kwao djml' ?? process.env.MAIL_PASSWORD,
-//       },
-//     },
-//     from: process.env.EMAIL_FROM,
-//   },
-//   url: {
-//     urlFront: process.env.APP_URL
-//   }
-// }
-
 const config_db = {
-  host: 'localhost',
-  username: "cpcoriente_rispark",
-  password: "Rispark_:bd2024",
-  database: "cpcoriente_risparkbd",
-  dialect: "mysql",
-  secret: 'das09df987y3nd0983nfzzqair3fd34',
+  host: process.env.HOST ?? 'localhost',
+  username: process.env.USERDB ?? "root",
+  password: process.env.PASSDB ?? "",
+  database: process.env.DATABASE ?? "rispark",
+  dialect: process.env.DIALECT ?? "mysql",
+  secret: process.env.SECRETKEYJWT ?? '',
   urlFiles: `http://${process.env.HOST}:${process.env.PORT}/api/archivos/`,
   email: {
     smtp: {
