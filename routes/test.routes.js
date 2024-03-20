42a6f6b3e38e648e1e8b2f2ad42dd83b3e0b6d39
juanPHome <juan.martinez@cpcoriente.org>
2024-03-20 09:49:43 -0500

    :hammer:

--- routes/test.routes.js
+++ routes/test.routes.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
+const GeneralsCTR = require('../controllers/generals.controller');
+
+const generalsController = new GeneralsCTR();
 
 const router = Router();
 
-router.get("/", async (req, res) => {
-    res.status(200).json({ data: true, msg: 'test' });
-});
+router.get("/", async (req, res) => await generalsController.getStadisticsSoftware(req, res));
   
 module.exports = router;
