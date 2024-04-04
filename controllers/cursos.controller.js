18f78c010aa58623a2f0954aa3856832683394d4
juanPabloM <juan.martinez@cpcoriente.org>
2024-04-04 10:22:41 -0500

    :hammer:

--- controllers/cursos.controller.js
+++ controllers/cursos.controller.js
@@ -150,6 +150,7 @@ class CursosCTR {
           attributes: [
             'id',
             'nombre',
+            'estado',
             [literal(`(SELECT COUNT(1) FROM cursos_clases AS cc WHERE cc.id_curso_sesion = sesiones.id AND cc.estado = 1)`), 'totalClases']
           ],
           where: {
